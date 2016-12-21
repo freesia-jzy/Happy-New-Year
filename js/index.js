4792c40e430aa526fd07125259c8395c78ba4018
grace <1253110928@qq.com>
2016-12-21 17:19:03 +0800

    页面转换，css3完善

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@
  */
 window.onload=function() {
     var music = document.getElementById("music");
+    var page1=document.getElementById("page1");
+    var page2=document.getElementById("page2");
+    var page3=document.getElementById("page3");
     var audio=document.getElementsByTagName("audio")[0];
     //当音乐播放完停止时，自动停止光盘旋转
     audio.addEventListener("ended",function (event) {
@@ -27,4 +30,14 @@ window.onload=function() {
             //this.style.webkitAnimationPlayState="paused";
         }
     },false);
+    page1.addEventListener("touchstart",function (event) {
+        page1.style.display="none";
+        page2.style.display="block";
+        page3.style.display="block";
+        page3.style.top="100%";
+        setTimeout(function () {
+            page2.setAttribute("class","page fadeOut");
+            page3.setAttribute("class","page fadeIn");
+        },5500)
+    },false)
 };
